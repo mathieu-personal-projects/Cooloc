8c8a9f643cdd9afcad70a151c9ea204857b5e8fa
Mathieu Audibert <mathieu.audibert27@gmail.com>
2026-07-24 00:01:56 +0200

    feat: .env and other changes

--- back/src/api/v2/router.py
+++ back/src/api/v2/router.py
@@ -2,6 +2,7 @@
 from fastapi import APIRouter
 from src.api.v2.endpoints import hello 
 
+
 api_router = APIRouter()
 
 api_router.include_router(hello.router, prefix="/hello", tags=["hello"])
--- back/src/config.py
+++ back/src/config.py
@@ -1,12 +1,22 @@
 from functools import lru_cache
 from pydantic_settings import BaseSettings, SettingsConfigDict
+from pathlib import Path
 
+
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+ENV_FILE = BASE_DIR / ".docker/.env"
 
 class Settings(BaseSettings):
     APP_NAME: str = "Cooloc's API"
     DEBUG: bool = False
     VERSION: str = "v2"
     API_PREFIX: str = "/api/" + VERSION
+
+    model_config = SettingsConfigDict(
+        env_file=ENV_FILE,
+        env_file_encoding="utf-8",
+        extra="ignore"
+    )
 
 @lru_cache
 def get_settings() -> Settings:
