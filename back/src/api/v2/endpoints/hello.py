2b147ccdbc0f1423636c6c5165430d89476c0520
Mathieu Audibert <mathieu.audibert27@gmail.com>
2026-07-24 17:33:32 +0200

    fix(hello): hello world endpoint now pings db

--- back/src/api/v2/endpoints/hello.py
+++ back/src/api/v2/endpoints/hello.py
@@ -1,7 +1,8 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from typing import Dict
 from src.models.api.ApiResponse import ApiResponse
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import text
 from src.core.db_connection import get_db
 
 
@@ -9,11 +10,23 @@ router = APIRouter()
 
 @router.get("/", response_model=ApiResponse[dict])
 async def get_hello(db: AsyncSession = Depends(get_db)) -> ApiResponse[dict]:
-    return ApiResponse(
-        statusCode=200,
-        message="successfully called the Hello World endpoint",
-        data={
-            "Hello": "World!",
-            "dbStatus": "OK"
-        }
-    )
+    try: 
+        await db.execute(text("SELECT 1"))
+        
+        return ApiResponse(
+                statusCode=200,
+                message="successfully called the Hello World endpoint",
+                data={
+                    "Hello": "World!",
+                    "dbStatus": "OK"
+                }
+            )
+
+    except Exception as e:
+        return ApiResponse(
+                statusCode=500,
+                message="Unable to call Hello World and the db",
+                data={
+                    "errorMessage": e
+                }
+            )
