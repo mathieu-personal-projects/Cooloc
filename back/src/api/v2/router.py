bcea5258a93a57d544ed8d9d82f8c41f182300ee
Mathieu Audibert <mathieu.audibert27@gmail.com>
2026-07-24 18:46:03 +0200

    feat(router): adding register endpoint

--- back/src/api/v2/router.py
+++ back/src/api/v2/router.py
@@ -1,8 +1,10 @@
 from fastapi import APIRouter
 from src.api.v2.endpoints import hello 
+from src.api.v2.endpoints.auth import register
 
 
 api_router = APIRouter()
 
 api_router.include_router(hello.router, prefix="/hello", tags=["hello"])
 
+api_router.include_router(register.router, prefix="/auth", tags=["auth"])
