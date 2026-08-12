dc17e6d85db5b4aceeb5e2396593cb6a468a037d
Mathieu Audibert <mathieu.audibert27@gmail.com>
2026-08-12 22:48:42 +0200

    fix: whitespace

--- back/src/core/db_connection.py
+++ back/src/core/db_connection.py
@@ -2,6 +2,7 @@ from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
 from src.config import settings
 
+
 engine = create_async_engine(settings.DB_URL, echo=settings.DEBUG)
 
 AsyncSessionLocal = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
